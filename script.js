82642b3f7aba5748a377bcbc38756c8e9283c3f5
Mar <mariojurendic@gmail.com>
2022-10-11 19:26:06 -0400

    Some css changes and Added a little Javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+// Elements
+
+const form = document.getElementById("form");
+
+let firstName = document.querySelector("firstName");
+let lastName = document.querySelector("lastName");
+let email = document.querySelector("emailAddress");
+let phoneNum = document.querySelector("phone");
+
+const submit = document.querySelector("submit-btn");
