9a4c592f6eb804ea7b44d267e76cfc54288f9b45
Mar <mariojurendic@gmail.com>
2022-11-16 20:17:48 -0500

    Yeah haven't been on for a while. Well whatever back now
    Added a function where it will store data in an object
    could have added localstorage but didn't want too.

--- script.js
+++ script.js
@@ -3,9 +3,44 @@
 
 const form = document.getElementById("form");
 
-let firstName = document.querySelector("firstName");
-let lastName = document.querySelector("lastName");
-let email = document.querySelector("emailAddress");
-let phoneNum = document.querySelector("phone");
+// // let firstName = document.querySelector("firstName");
+// let lastName = document.querySelector("lastName");
+// let email = document.querySelector("emailAddress");
+// let phoneNum = document.querySelector("phone");
 
-const submit = document.querySelector("submit-btn");
+// // const submit = document.querySelector("submit-btn");
+
+let userData = [];
+
+const addUserData = (ev) => {
+  ev.preventDefault();
+  let userInput = {
+    firstName: document.getElementById("input-fname").value,
+    lastName: document.getElementById("input-lname").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone-numb").value,
+  };
+
+  if (
+    userInput.firstName &&
+    userInput.lastName &&
+    userInput.email &&
+    userInput.phone
+  ) {
+    userData.push(userInput);
+    document.forms[0].reset();
+  } else {
+    window.alert("Please fill all fields");
+    location.reload();
+  }
+  // console.warn("added", { userData });
+  // let pre = document.querySelector("#msg pre");
+  // pre.textContent = "\n" + JSON.stringify(userData, "\t", 2);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("submit-btn").addEventListener("click", addUserData);
+});
+
+console.log(userData);
+console.log(typeof firstName);
